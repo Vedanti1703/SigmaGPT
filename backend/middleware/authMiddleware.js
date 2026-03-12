14befe8167cc26346df3cb60d4b4204b01706942
Vedanti1703 <vedantighanekar1703@gmail.com>
2026-03-12 22:53:52 +0530

    fixed user specific thread fetching

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,18 +3,30 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = "mysupersecretkeyisVedanti1703";
 
 export default function authMiddleware(req, res, next) {
+
   const authHeader = req.headers.authorization;
 
-  if (!authHeader)
+  if (!authHeader) {
     return res.status(401).json({ error: "No token provided" });
+  }
 
   const token = authHeader.split(" ")[1];
 
+  if (!token) {
+    return res.status(401).json({ error: "Token missing" });
+  }
+
   try {
+
     const decoded = jwt.verify(token, JWT_SECRET);
+
     req.user = decoded;
+
     next();
+
   } catch (err) {
+
     return res.status(401).json({ error: "Invalid token" });
+
   }
 }
